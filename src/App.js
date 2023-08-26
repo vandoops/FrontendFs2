14ab2f67ece020c9932fd668729b7c13123fce37
vandoops <100371881+vandoops@users.noreply.github.com>
2023-08-26 02:49:57 -0300

    dploy atualização

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import TelaCadastroPatrimonio from "./telas/TelaCadastroPatrimonio.jsx";
-import TelaCadastrocategoria from "./telas/TelaCadastroCategoria.jsx";
 import TelaCadastrofuncao from "./telas/TelaCadastroFuncao.jsx";
-import TelaCadastroevento from "./telas/TelaCadastroEvento.jsx";
-import TelaCadPessoa from "./telas/TelaCadPessoa.jsx";
 import TelaCadastroFinanceiro from "./telas/TelaCadastroFinanceiro.jsx";
 import TelaMenu from "./telas/TelaMenu";
 import Tela404 from "./telas/Tela404";
@@ -14,11 +10,7 @@ function App() {
     <div >
       <BrowserRouter>
         <Routes>
-          <Route path="/patrimonio" element={<TelaCadastroPatrimonio/>}/>
-          <Route path="/categoria" element={<TelaCadastrocategoria/>}/> 
           <Route path="/funcao" element={<TelaCadastrofuncao/>}/>
-          <Route path="/evento" element={<TelaCadastroevento/>}/>         
-          <Route path="/pessoas" element={<TelaCadPessoa/>}/>
           <Route path="/financas" element={<TelaCadastroFinanceiro/>}/>
           <Route path="/" element={<TelaMenu/>}/>
           <Route path="*" element={<Tela404/>} />
