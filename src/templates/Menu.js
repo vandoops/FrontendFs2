4e30ee33f15273e058637c449705ad651b7fb4d0
vandoops <100371881+vandoops@users.noreply.github.com>
2023-08-26 02:59:46 -0300

    ..

--- src/templates/Menu.js
+++ src/templates/Menu.js
@@ -16,25 +16,14 @@ export default function Menu(props) {
           <Nav className="me-auto">
             <NavDropdown title="Cadastros" id="basic-nav-dropdown">
               <div>
-                <LinkContainer to="/pessoas">
-                  <NavDropdown.Item>Pessoas</NavDropdown.Item>
-                </LinkContainer>
-                <NavDropdown.Divider />
+                
                 <LinkContainer to="/funcao">
-                  <NavDropdown.Item>Cargos</NavDropdown.Item>
+                  <NavDropdown.Item>Tipo de movimentação</NavDropdown.Item>
                 </LinkContainer>
                 <NavDropdown.Divider />
-                <LinkContainer to="/patrimonio">
-                  <NavDropdown.Item>Patrimônio</NavDropdown.Item>
-                </LinkContainer>
-                <NavDropdown.Divider />
-                <LinkContainer to="/categoria">
-                  <NavDropdown.Item>Tipo de Patrimonio</NavDropdown.Item>
-                </LinkContainer>
-                <NavDropdown.Divider />
-                <LinkContainer to="/evento">
-                  <NavDropdown.Item>Eventos</NavDropdown.Item>
-                </LinkContainer>
+                
+                
+                
                 <NavDropdown.Divider />
                 <LinkContainer to="/financas">
                   <NavDropdown.Item>Financeiro</NavDropdown.Item>
