fdb3e40aa6947f44802a9935323c7b019c89bca1
vandoops <100371881+vandoops@users.noreply.github.com>
2023-09-24 01:26:09 -0300

    teste

--- src/telas/TelaCadastroMovimentacao.jsx
+++ src/telas/TelaCadastroMovimentacao.jsx
@@ -6,6 +6,7 @@ import { Alert, Container } from "react-bootstrap";
 //import { urlBase } from "../utilitarios/definicoes";
 
 
+
 export default function TelaCadastroMovimentacao(props) {
     const [exibirTabela, setExibirTabela] = useState(true);
     const [movimentacoes, setMovimentacao] = useState([]);
